a78260bff1f1f39a0d6b5bdd68ba88e6bbf377cf
Mohamed Raed <mohamed.raed.2022o@gmail.com>
2026-01-29 01:34:32 +0200

    update 3  index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -30,7 +30,7 @@ app.use(cors({
 app.use(express.json())
 app.use(cookieParser())
 
-app.get('/',(req , res)=> res.json({message:"API is running..."}))
+app.get('/api',(req , res)=> res.json({message:"API is running..."}))
 
 app.use('/api/admin/auth' , authRoutes)
 app.use('/api/admin/users' , adminUsersRoutes)
