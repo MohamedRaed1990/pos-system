912de200a2cb280a436c377a89d88300a5ce3dff
Mohamed Raed <mohamed.raed.2022o@gmail.com>
2026-01-29 02:06:55 +0200

    update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL:'http://localhost:5000/api',
+    baseURL:'https://pos-system-4yqe.vercel.app/api',
     withCredentials:true
 });
 
