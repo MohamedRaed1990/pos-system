ff98bc55e213c1b70606810876122e51f96019a6
Mohamed Raed <mohamed.raed.2022o@gmail.com>
2026-02-06 01:08:48 +0200

    update on invoiceRoutes.js #1

--- backend/routes/invoiceRoutes.js
+++ backend/routes/invoiceRoutes.js
@@ -4,17 +4,15 @@ import {
     getInvoices,
     getInvoiceById,
 } from '../controllers/invoiceController.js'
-// import { protect , authorize } from '../middlewares/auth.js'
-import { protectAdmin , authorizeAdmin } from '../middlewares/adminAuth.js'
+import { protect , authorize } from '../middlewares/auth.js'
+
 const router = express.Router();
 
 
-// router.get('/', protect ,getInvoices)
-// router.get('/:id', protect ,getInvoiceById)
-// router.post('/',protect,authorize('manager','admin'),createInvoice)
-router.get('/', protectAdmin ,getInvoices)
-router.get('/:id', protectAdmin ,getInvoiceById)
-router.post('/',protectAdmin,authorizeAdmin('manager','admin'),createInvoice)
+router.get('/', protect ,getInvoices)
+router.get('/:id', protect ,getInvoiceById)
+router.post('/',protect,authorize('manager','admin'),createInvoice)
+
 
 
 export default router
